a7a9182c3d51ad7a7504c9750f3db2fc2e1dd005
jotape-exe <acontamitoqeuperdi2@gmail.com>
2023-04-08 00:42:12 -0300

    Product Classes

---
+++ src/main/java/com/computershop/app/model/Product.java
@@ -0,0 +1,75 @@
+package com.computershop.app.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "product")
+public class Product {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "product_name")
+    private String name;
+
+    @Column(name = "product_desc")
+    private String description;
+
+    @Column(name = "manufacturer")
+    private String manufacturer;
+
+    @Column(name = "value")
+    private Double value;
+
+    @Column(name = "amout")
+    private Integer amount;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public Double getValue() {
+        return value;
+    }
+
+    public void setValue(Double value) {
+        this.value = value;
+    }
+
+    public Integer getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Integer amount) {
+        this.amount = amount;
+    }
+}
---
+++ src/main/java/com/computershop/app/service/impl/ProductService.java
@@ -0,0 +1,33 @@
+package com.computershop.app.service.impl;
+
+import com.computershop.app.model.Product;
+import com.computershop.app.service.CrudService;
+
+import java.util.ArrayList;
+
+public class ProductService implements CrudService<Product> {
+    @Override
+    public Product findById(Long id) {
+        return null;
+    }
+
+    @Override
+    public ArrayList<Product> findAll() {
+        return null;
+    }
+
+    @Override
+    public Product create(Product product) {
+        return null;
+    }
+
+    @Override
+    public Product update(Product product) {
+        return null;
+    }
+
+    @Override
+    public void delete(Long id) {
+
+    }
+}
