64f2fceae7045cfd7f44d14a601102a9209bc534
jotape-exe <acontamitoqeuperdi2@gmail.com>
2023-04-10 12:11:15 -0300

    New Exceptions

---
+++ src/main/java/com/computershop/app/service/exceptions/ObjectNotFoundException.java
@@ -0,0 +1,13 @@
+package com.computershop.app.service.exceptions;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class ObjectNotFoundException extends EntityNotFoundException {
+    public ObjectNotFoundException(String message) {
+        super(message);
+    }
+
+}
