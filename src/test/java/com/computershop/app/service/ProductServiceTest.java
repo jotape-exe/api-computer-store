2e1613f6ea444c54e1f62839dc3abd71c8a587d2
jotape-exe <acontamitoqeuperdi2@gmail.com>
2023-04-30 13:32:14 -0300

    Setup and verify

--- src/test/java/com/computershop/app/service/ProductServiceTest.java
+++ src/test/java/com/computershop/app/service/ProductServiceTest.java
@@ -4,6 +4,7 @@ import com.computershop.app.AppApplicationTests;
 import com.computershop.app.model.Product;
 import com.computershop.app.repository.ProductRepository;
 import com.computershop.app.service.impl.ProductService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentMatchers;
@@ -20,10 +21,22 @@ public class ProductServiceTest extends AppApplicationTests {
     @Autowired
     private ProductService productService;
 
+    private Product product;
+
+    @BeforeEach
+    public void setUp() {
+        product = new Product();
+        product.setId(1L);
+        product.setAmount(12);
+        product.setName("Test Product");
+        product.setDescription("Description test");
+        product.setManufacturer("Test Manufacturer");
+        product.setValue(333.30);
+    }
+
     @Test
     @DisplayName("Deve retornar um produto valido")
     public void deveRetornarUmProdutoValido(){
-        Product product = createProduct();
         Mockito.when(productRepository.save(ArgumentMatchers.eq(product))).thenReturn(product);
 
         productService.create(product);
@@ -32,26 +45,11 @@ public class ProductServiceTest extends AppApplicationTests {
     @Test
     @DisplayName("Deve apagar um produto")
     public void deveApagarProduto(){
-
         Long productId = 1L;
 
-        Product product = createProduct();
-
         Mockito.when(productRepository.findById(ArgumentMatchers.eq(productId))).thenReturn(Optional.of(product));
-
         productService.delete(productId);
-    }
 
-    private Product createProduct(){
-        Product product = Mockito.mock(Product.class);
-
-        Mockito.when(product.getId()).thenReturn(1L);
-        Mockito.when(product.getAmount()).thenReturn(12);
-        Mockito.when(product.getName()).thenReturn("Test Product");
-        Mockito.when(product.getDescription()).thenReturn("Description test");
-        Mockito.when(product.getManufacturer()).thenReturn("Test Manufacturer");
-        Mockito.when(product.getValue()).thenReturn(333.30);
-
-        return product;
+        Mockito.verify(productRepository, Mockito.times(1)).delete(ArgumentMatchers.any(Product.class));
     }
 }
