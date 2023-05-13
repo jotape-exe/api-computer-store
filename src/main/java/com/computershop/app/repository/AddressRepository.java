02e3158ceff89146d6ed643d534f714b730ed185
jotape-exe <acontamitoqeuperdi2@gmail.com>
2023-05-13 17:55:56 -0300

    FindByCep Query

--- src/main/java/com/computershop/app/repository/AddressRepository.java
+++ src/main/java/com/computershop/app/repository/AddressRepository.java
@@ -2,8 +2,13 @@ package com.computershop.app.repository;
 
 import com.computershop.app.model.Address;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface AddressRepository extends JpaRepository<Address, String> {
+public interface AddressRepository extends JpaRepository<Address, Long> {
+    //@Query(value = "SELECT * FROM ADDRESS WHERE CEP = ?1", nativeQuery = true)
+    Optional<Address> findByCep(String cep);
 }
